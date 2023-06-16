a65cbbb9fbf3f53b194804160fb985e0f33b6881
xieshixin <aojiaodage@gmail.com>
2023-06-16 11:03:53 +0800

    20230616110353 更新文章  M articles/33.html  M css/article.css  M js/lazy.js

--- js/lazy.js
+++ js/lazy.js
@@ -1,40 +1,65 @@
-// 获取所有图片
-const imgList = document.querySelectorAll('img');
-// 用于记录当前显示到了哪一张图片
-let index = 0;
-function lazyLoad() {
-    // 获取浏览器视口高度,这里写在函数内部是考虑浏览器窗口大小改变的情况
-    const viewPortHeight = window.innerHeight || document.documentElement.clientHeight
-    for (let i=index; i<imgList.length; i++) {
-        let dataSrc = imgList[i].getAttribute('data-src');
+var imgList = document.querySelectorAll('img');
+var imgMap = {};
+
+if (imgList && imgList.length) {
+    for (var i=0; i<imgList.length; i++) {
+        var item = imgList[i];
+        var dataSrc = item.getAttribute('data-src');
         if (!dataSrc) {
             continue;
         }
-
-        // 如果有display为none的，就显示了（媒体查询处隐藏的）
-        let cssObj = window.getComputedStyle(imgList[i].parentNode);
+        // 如果有其父节点display为none，则不加载（媒体查询处隐藏的，但只能控制其父，若是父的父元素就没有办法了，还得继续加逻辑判断）
+        var cssObj = window.getComputedStyle(item.parentNode);
         if (cssObj && cssObj.display === 'none') {
-            return;
+            continue;
         }
-        // 这里用可视区域高度减去图片顶部距离可视区域顶部的高度
-        const distance = viewPortHeight - imgList[i].getBoundingClientRect().top;
-        // 如果可视区域高度大于等于元素顶部距离可视区域顶部的高度，说明图片已经出现在了视口范围内
-        if (distance >= 0) {
-            // 给图片赋值真实的src，展示图片
-            imgList[i].src = dataSrc;
-            // 前i张图片已经加载完毕，下次从第i+1张开始检查是否需要显示
-            index = i + 1;
+        imgMap[i] = {
+            img: item,
+            dataSrc: dataSrc,
+            key: i
+        };
+    }
+}
+
+function getUnhandled() {
+    var imgList = [];
+    for (var key in imgMap) {
+        imgList.push(imgMap[key]);
+    }
+    return imgList;
+}
+
+function lazyLoad() {
+    // 获取浏览器视口高度,这里写在函数内部是考虑浏览器窗口大小改变的情况
+    var unhandled = getUnhandled();
+    if (unhandled.length) {
+        var viewPortHeight = window.innerHeight || document.documentElement.clientHeight;
+        for (var i=0; i<unhandled.length; i++) {
+            var item = unhandled[i];
+            // 这里用可视区域高度减去图片顶部距离可视区域顶部的高度
+            var distance = viewPortHeight - item.img.getBoundingClientRect().top;
+            // 如果可视区域高度大于等于元素顶部距离可视区域顶部的高度，说明图片已经出现在了视口范围内
+            if (distance >= 0) {
+                // 给图片赋值真实的src，展示图片
+                item.img.src = item.dataSrc;
+                delete imgMap[item.key];
+            }
         }
     }
 }
 
 // 定义一个防抖函数
-function debounce(fn, delay = 500) {
-    let timer = null;
-    return function (...args) {
+function debounce(fn, delay) {
+    if (!delay) {
+        delay = 500;
+    }
+    var timer = null;
+    return function () {
         if (timer) clearTimeout(timer);
-        timer = setTimeout(() => {
-            fn.apply(this, args);
+        var _args = arguments;
+        var that = this;
+        timer = setTimeout(function () {
+            fn.apply(that, _args);
         }, delay);
     };
 }
