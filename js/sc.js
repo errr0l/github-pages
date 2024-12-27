15acaf0a900f97f58418a66f6a0bb822343de9ac
埃罗 <aojiaodage@gmail.com>
2024-12-27 21:15:29 +0800

    20241227211529 更新文章  M index.html  M index_2.html  M index_3.html  M index_4.html  M index_5.html ?? js/sc.js

---
+++ js/sc.js
@@ -0,0 +1,35 @@
+const wrapper = document.querySelector('#header #sc-box .wrapper');
+
+function makeSideCard(list, opts) {
+    const { title, showingDate, dateName } = opts;
+    const sc = document.createElement('div');
+    sc.className = "side-card b-s-1 mg-b-10";
+    let html = "<div class='wrapper'><h2 class='item title'><span>"+ title +"</span></h2>";
+    html += "<div class='item content'>";
+    for (const item of list) {
+        html += "<div class='item1' style='word-break: break-all;'>";
+        let content;
+        if (showingDate) {
+            content = `${item[dateName]} ${item.title}`;
+        }
+        else {
+            content = item.title
+        }
+        html += `<a href='${item.url}'>${content}</a>`;
+        html += "</div>";
+    }
+    html += "</div>";
+    html += "</div>";
+    sc.innerHTML = html;
+    return sc;
+}
+
+const latestSideCard = makeSideCard(latest, {
+    title: "最近更新",
+    showingDate: true,
+    dateName: "updatedAt"
+});
+
+wrapper.appendChild(latestSideCard);
+
+
